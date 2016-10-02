68e1e39a90fdac7b7e18b9e3b9907bf054f6ceb2
Gregg <gregg@topshelfdesign.net>
2016-10-02 11:55:55 -0400

    small time work done on the welcome screen

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <div class="column">
+        <div class="callout large primary">
+            <h1>The Flower Store</h1>
+        </div>
+    </div>
+@endsection
